9e8c31a49b103ba85cc507dd24b00c5cb1edcc52
Reymond Caceres <reymond19890@gmail.com>
2023-08-29 20:41:22 -0400

    404

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <header className='header'>
         <nav className='navbar'> 
-            <a className='navbar__title' href="#"><img className='navbar__logo' src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty.png" alt="" /></a>
+            <a className='navbar__title' href="#"><img className='navbar__logo' src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty.png" alt="logo" /></a>
             
         </nav>
     </header>
